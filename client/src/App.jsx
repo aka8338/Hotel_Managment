191bed5b0c5abff7f8bcf36f7c94b312e85fdf4e
Aklilu Beyero <141402232+aka8338@users.noreply.github.com>
2025-02-11 09:35:57 +0300

    setupping tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1> Hello World</h1>
+      <h1 className="text-4xl font-bold text-center text-blue-500 mt-10">
+        Hello, Tailwind CSS!
+      </h1>
     </>
   );
 }
